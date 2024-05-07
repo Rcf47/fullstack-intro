983e50de3af38c3066247a89bc0112a2733dbfc8
Rcf47 <xenos26@gmail.com>
2024-05-07 23:20:22 +0700

    fix: fix bug with post id

--- app/page.js
+++ app/page.js
@@ -26,6 +26,7 @@ export default async function Home() {
           return (
             <Post
               key={post.id}
+              id={post.id}
               title={post.title}
               content={post.content}
               authorName={post.author.name}
