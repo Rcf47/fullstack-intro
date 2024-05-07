b08db766a884cbb163f68e2755917b19a807c089
Rcf47 <xenos26@gmail.com>
2024-05-07 08:13:33 +0700

    refactor: add styles for post component

--- app/components/Post/Post.jsx
+++ app/components/Post/Post.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./Post.module.css";
 
 function Post({ id, title, content, authorName }) {
   return (
     <div
       style={{ border: "1px solid #89B4FA", padding: "15px", margin: "0 10px" }}
+      className={styles.posts}
     >
       <h3>{authorName}</h3>
       <h4>{title}</h4>
