59fdd20e970ed601eea3a8dbcb35ea214c78a57d
Rcf47 <xenos26@gmail.com>
2024-05-07 08:13:50 +0700

    feat: add link to add post

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import prisma from "../lib/prisma";
 import Post from "./components/Post/Post";
+import Link from "next/link";
 
 async function getPosts() {
   const posts = await prisma.post.findMany({
@@ -18,18 +19,20 @@ export default async function Home() {
   const posts = await getPosts();
   return (
     <main className={styles.main}>
+      <Link href="/add-post">Add Post</Link>
       <h1 className={styles.title}>Feed</h1>
-      {posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            content={post.content}
-            authorName={post.author.name}
-            className={styles.posts}
-          />
-        );
-      })}
+      <div className={styles.posts}>
+        {posts.map((post) => {
+          return (
+            <Post
+              key={post.id}
+              title={post.title}
+              content={post.content}
+              authorName={post.author.name}
+            />
+          );
+        })}
+      </div>
     </main>
   );
 }
