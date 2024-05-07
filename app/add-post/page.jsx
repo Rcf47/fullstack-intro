e66b1484fc5811adf98ef6a8fff344af7611ab0d
Rcf47 <xenos26@gmail.com>
2024-05-07 09:02:23 +0700

    feat: add handle for add post

--- app/add-post/page.jsx
+++ app/add-post/page.jsx
@@ -1,9 +1,61 @@
 "use client";
+import styles from "@/app/page.module.css";
+import Link from "next/link";
+import { useState } from "react";
 
-export default function addPost() {
+export default function AddPost() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      fetch("api/add-post", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, content }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    setTitle("");
+    setContent("");
+  };
   return (
-    <main>
+    <main className={styles.main}>
+      <Link href="/">View Feed</Link>
       <h1>Add Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={handleTitleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="content">Content</label>
+          <textarea
+            id="content"
+            value={content}
+            onChange={handleContentChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 }
